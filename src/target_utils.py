922ad3ab38179251ebfa4919acfcf2659ebb7a8d
Antoine Honoré <honore@kth.se>
2019-11-05 09:26:16 +0100

    Allow to pass the expected number of class, avoid problems when no class is predicted

--- src/target_utils.py
+++ src/target_utils.py
@@ -2,8 +2,12 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
 
-def to_onehot(Y):
+def to_onehot(Y,n=None):
     enc = OneHotEncoder(handle_unknown='ignore')
-    enc.fit(np.unique(Y).reshape(-1, 1))
+    if n is None:
+        enc.fit(np.unique(Y).reshape(-1, 1))
+    else:
+        enc.fit(np.arange(n).reshape(-1,1))
+
     Y_ = enc.transform(Y[:, np.newaxis]).toarray()
     return Y_
